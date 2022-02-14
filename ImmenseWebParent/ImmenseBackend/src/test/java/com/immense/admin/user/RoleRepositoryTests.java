8fa6fe30b8fe1bb362125590f28bb0e84b5b3d16
David Mbaimbai <davymbaimbai@gmail.com>
2022-02-14 12:07:08 +0200

    Added tests for the Roles repository

--- ImmenseWebParent/ImmenseBackend/src/test/java/com/immense/admin/user/RoleRepositoryTests.java
+++ ImmenseWebParent/ImmenseBackend/src/test/java/com/immense/admin/user/RoleRepositoryTests.java
@@ -8,12 +8,16 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
 public class RoleRepositoryTests {
     @Autowired
     private RoleRepository roleRepository;
@@ -24,4 +28,18 @@ public class RoleRepositoryTests {
         Role savedRole = roleRepository.save(roleAdmin);
         assertThat(savedRole.getId()).isGreaterThan(0);
     }
+
+    @Test
+    public void testCreateRestRoles(){
+        Role roleSalesPerson = new Role("Salesperson",
+                "Manage product price, " + "customers,shipping, orders and sales report");
+        Role roleEditor = new Role("Editor",
+                "Manage categories , brands," + "products, articles and menus");
+        Role roleShipper = new Role("Shipper",
+                "Manage view products,view order and update status");
+        Role roleAssistant = new Role("Assistant",
+                "Manage questions and reviews");
+       roleRepository.saveAll(List.of(roleSalesPerson,roleEditor,roleShipper,roleAssistant));
+    }
+
 }
