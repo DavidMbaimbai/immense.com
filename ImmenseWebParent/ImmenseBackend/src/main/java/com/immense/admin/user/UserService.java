23f4b5f12c0000ab5b64900229f687e99902e6dd
David Mbaimbai <davymbaimbai@gmail.com>
2022-02-15 12:45:32 +0200

    Created user page

--- ImmenseWebParent/ImmenseBackend/src/main/java/com/immense/admin/user/UserService.java
+++ ImmenseWebParent/ImmenseBackend/src/main/java/com/immense/admin/user/UserService.java
@@ -1,6 +1,8 @@
 package com.immense.admin.user;
 
+import com.immense.admin.user.user.RoleRepository;
 import com.immense.admin.user.user.UserRepository;
+import com.immense.common.entity.Role;
 import com.immense.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,10 +13,18 @@ import java.util.List;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
     public List<User> listAllUsers(){
         return (List<User>) userRepository.findAll();
 
     }
+    public List<Role> listRoles(){
+        return (List<Role>) roleRepository.findAll();
+    }
 
+    public void save(User user) {
+        userRepository.save(user);
+    }
 }
--- ImmenseWebParent/ImmenseBackend/src/main/java/com/immense/admin/user/user/UserController.java
+++ ImmenseWebParent/ImmenseBackend/src/main/java/com/immense/admin/user/user/UserController.java
@@ -1,15 +1,42 @@
 package com.immense.admin.user.user;
 
 import com.immense.admin.user.UserService;
+import com.immense.common.entity.Role;
+import com.immense.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
     @Autowired
     private UserService userService;
-    public String listAllUsers(){
+    @GetMapping("/users")
+    public String listAllUsers(Model model){
+        List<User> listUsers=userService.listAllUsers();
+
+        model.addAttribute("listUsers", listUsers);
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model){
+        List<Role> listRoles = userService.listRoles();
+        User user = new User();
+        user.setEnabled(true);
+        model.addAttribute("user", user);
+        model.addAttribute("listRoles", listRoles);
+        return "user_form";
+    }
+    @PostMapping("/users/save")
+    public String saveUser(User user){
+        System.out.println(user);
+        userService.save(user);
+        return "redirect:/users";
     }
 
 }
